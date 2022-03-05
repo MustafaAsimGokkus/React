2e13fdcf2f65d155cd20336272ac3ef7afbd7f66
Asim Gokkus <asimgokkus1@gmail.com>
2022-03-05 10:48:08 +0100

    fake API

--- react-app/16-usestate-hook/src/components/App.jsx
+++ react-app/16-usestate-hook/src/components/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return <div className="container">
     <h1>{count}</h1>
-    <button onClick={() => { setCount(count + 1) }}>+</button>
+    <button onClick={() => { setCount(count + 1 ) }}>+</button>
     <button onClick={decrease}>-</button>
     <button className="reset-button" onClick={() => { setCount(0) }}>Reset</button>
     <br />
